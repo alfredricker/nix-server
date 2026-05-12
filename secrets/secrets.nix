098705046af21396c6dcbfa818309bc4f11b32ef
alfred ricker <alfred.ricker7@gmail.com>
2026-05-11 22:11:32 -0700

    secrets update

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   # Get these with: ssh-keyscan -t ed25519 <hostname>
   # Paste only the key portion (third field on the line).
-  main-node = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA4vosrFkfvLRcdKzIuXGbG78OhZ+RUs4fhuNE8X/nfP";
+  main-node = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ0rRSmoyEn8ed8LKuN+ZSXlcDm1ND+KeCPDnLlNFsPD";
 
   # Add media-node host keys here as you provision them:
   # la-node  = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAA REPLACE";
@@ -10,7 +10,7 @@ let
   # Your personal key — allows you to re-encrypt secrets from your machine.
   fred = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIME9Bmh6fg68kew2hciqg+gKIqhw0/vBB76i7UQlkAIE";
   # media node 1
-  freds-node = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA4vosrFkfvLRcdKzIuXGbG78OhZ+RUs4fhuNE8X/nfP";
+  freds-node = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEF08su0Yh9/eJyc+aVlEqGSipVIK20xAbxfxH4P00KO";
 
   allNodes = [ main-node freds-node ];
 in
