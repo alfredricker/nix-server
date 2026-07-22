226bc77d01fcb39fbc0f99bbe9505bbe4c546f90
alfred ricker <alfred.ricker7@gmail.com>
2026-07-22 15:16:55 -0400

    media node update

--- media-node.nix
+++ media-node.nix
@@ -4,4 +4,10 @@
 
   # Flirc USB IR dongle — used to program the universal remote for TV control.
   hardware.flirc.enable = true;
+
+  # Prevent USB autosuspend from resetting the dongle into a bad state,
+  # which otherwise looks like the remote randomly losing its programming.
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="20a0", TEST=="power/control", ATTR{power/control}="on"
+  '';
 }
