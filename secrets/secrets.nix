21f21e03ba3851e62af647674d274a4396548e4f
alfred ricker <alfred.ricker7@gmail.com>
2026-08-17 17:06:20 -0700

    consolidate dream trader

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -24,9 +24,5 @@ in
   "cloudflare-tunnel-docmost.age".publicKeys           = [ fred main-node ];
   "postgres-docmost-password.age".publicKeys           = [ fred main-node ];
   "docmost-env.age".publicKeys                         = [ fred main-node ];
-  "dream-trader-db-passwords.age".publicKeys           = [ fred main-node ];
   "dream-trader-runner-env.age".publicKeys             = [ fred main-node ];
-  "dream-trader-worker-env.age".publicKeys             = [ fred main-node ];
-  "dream-trader-watchdog-env.age".publicKeys           = [ fred main-node ];
-  "dream-trader-discord-webhook.age".publicKeys        = [ fred main-node ];
 }
